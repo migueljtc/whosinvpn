a2ede17957f857b5334c878d6d15bc4da7adf97d
migueljtc <miguelseabra@gmail.com>
2020-10-02 14:34:39 +0100

    Add files via upload
    Adaptations after running static analysis checker

--- rtvpn.py
+++ rtvpn.py
@@ -1,63 +1,65 @@
 import time
 
-find_me_in = "Log In"
-find_me_out = "Log Out"
-usr_name_string = "usrName"
+FIND_ME_IN = "Log In"
+FIND_ME_OUT = "Log Out"
+USR_NAME_STRING = "usrName"
 
-html_file_name = "index.html"
+HTML_FILE_NAME = "index.html"
 
-current_logged_users = []
-nr_logged_users = 0
+CURRENT_LOGGED_USERS = []
+NR_LOGGED_USERS = 0
 
 
 def follow(thefile):
-    thefile.seek(0,2)
+    thefile.seek(0, 2)
+
     while True:
-        line = thefile.readline()
-        if not line:
+        in_line = thefile.readline()
+        if not in_line:
             time.sleep(0.1)
             continue
-        yield line
+        yield in_line
+
 
 def add_user(raw_line):
-    global current_logged_users
-    global nr_logged_users 
+    global CURRENT_LOGGED_USERS
+    global NR_LOGGED_USERS
 
     user = process_line_in(raw_line)
-    print ("add: Processing user: " + user)
+    print("add: Processing user: " + user)
 
-    #make sure not to add the same user twice
-    if user not in current_logged_users:
-        current_logged_users.append(user)
-        nr_logged_users = nr_logged_users + 1
-        update_html(nr_logged_users, current_logged_users)
-        print ("Added user: " + user )
+    # make sure not to add the same user twice
+    if user not in CURRENT_LOGGED_USERS:
+        CURRENT_LOGGED_USERS.append(user)
+        NR_LOGGED_USERS = NR_LOGGED_USERS + 1
+        update_html(NR_LOGGED_USERS, CURRENT_LOGGED_USERS)
+        print("Added user: " + user)
 
-    print("Active users: " + str(nr_logged_users))
+    print("Active users: " + str(NR_LOGGED_USERS))
+
 
 def remove_user(raw_line):
-    global current_logged_users
-    global nr_logged_users
+    global CURRENT_LOGGED_USERS
+    global NR_LOGGED_USERS
     user = process_line_out(raw_line)
-    
-    print ("remove: Processing user: " + user)
+    print("remove: Processing user: " + user)
 
-    #avoid removing non-existant elements
-    if user in current_logged_users:
-        current_logged_users.remove(user)
-        #make sure it's allways >=0 or removed users that don't exist yet
-        if nr_logged_users > 0:
-            nr_logged_users = nr_logged_users - 1
-            update_html(nr_logged_users, current_logged_users)
-            print ("Removed user: " + user )
+    # avoid removing non-existant elements
+    if user in CURRENT_LOGGED_USERS:
+        CURRENT_LOGGED_USERS.remove(user)
+        # make sure it's allways >=0 or removed users that don't exist yet
+        if NR_LOGGED_USERS > 0:
+            NR_LOGGED_USERS = NR_LOGGED_USERS - 1
+            update_html(NR_LOGGED_USERS, CURRENT_LOGGED_USERS)
+            print("Removed user: " + user)
 
-    print("Active users: " + str(nr_logged_users))
+    print("Active users: " + str(NR_LOGGED_USERS))
 
 
-def update_html(nr_logged_users, current_logged_users):
-    user_list_str = '\n'.join(current_logged_users)
-     
-    header =''' <html>
+def update_html(_nr_logged_users, _current_logged_users):
+    user_list_str = '\n'.join(_current_logged_users)
+
+    header = '''<html>
   <head>
   <title>VPN Users Online: </title>
   <meta http-equiv="refresh" content="3"
@@ -66,34 +68,35 @@ def update_html(nr_logged_users, current_logged_users):
   <h2>VPN Users Online : {}</h2>
   <h5> {} </h5>
   </body>
-  </html>'''.format(nr_logged_users, user_list_str)
+  </html>'''.format(_nr_logged_users, user_list_str)
 
-    with open(html_file_name, 'w') as out:
+    with open(HTML_FILE_NAME, 'w') as out:
         out.write(header + '\n')
 
 
 def process_line_in(raw_line):
-    index_start =  raw_line.find('usrName')
+    index_start = raw_line.find('usrName')
     index_end = raw_line.find("attackStatus")
-    return raw_line[index_start+8:index_end].strip()
+    return raw_line[index_start + 8:index_end].strip()
+
 
 def process_line_out(raw_line):
-    index_start =  raw_line.find('usrName')
+    index_start = raw_line.find('usrName')
     index_end = raw_line.find("ifdir")
-    return raw_line[index_start+8:index_end].strip()
+    return raw_line[index_start + 8:index_end].strip()
 
 
 if __name__ == '__main__':
-    logfile = open("x.log","r")
+    logfile = open("x.log", "r")
     loglines = follow(logfile)
 
-    #clean up html on start 
-    #update_html(nr_logged_users, current_logged_users)
+    # clean up html on start
+    # update_html(NR_LOGGED_USERS, CURRENT_LOGGED_USERS)
 
     for line in loglines:
-        if find_me_in in line: 
-            #print (line)
-            add_user (line)
-        if find_me_out in line:
-            #print (line)
-            remove_user (line)
+        if FIND_ME_IN in line:
+            # print (line)
+            add_user(line)
+        if FIND_ME_OUT in line:
+            # print (line)
+            remove_user(line)
