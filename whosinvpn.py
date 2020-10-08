dd0ab6234e9ce4fb3248ce85a33a94048d452a61
migueljtc <miguelseabra@gmail.com>
2020-10-08 20:35:41 +0100

    Added argument parsing and IO error handling

--- whosinvpn.py
+++ whosinvpn.py
@@ -1,15 +1,15 @@
 import time
 from datetime import datetime
+import argparse
+import sys
 
 FIND_ME_IN = "Log In"
 FIND_ME_OUT = "Log Out"
 USR_NAME_STRING = "usrName"
-
 HTML_FILE_NAME = "index.html"
-
 CURRENT_LOGGED_USERS = []
 NR_LOGGED_USERS = 0
-START_UP_TIME =""
+START_UP_TIME = ""
 
 
 def follow(thefile):
@@ -86,7 +86,8 @@ def update_html(_nr_logged_users, _current_logged_users):
   <meta http-equiv="refresh" content="3"
   </head>
   <body>
-  <h2>VPN Users Online : {}</h2> <h3>Time: {} </h3><h5> Online since: {}</h5>
+  <h2>VPN Users Online : {}</h2> <h3> Last Refresh: {}
+  </h3><h5> Online since: {}</h5>
   <h5> {} </h5>
   </body>
   </html>'''.format(_nr_logged_users, t_now, START_UP_TIME, user_list_str)
@@ -118,11 +119,26 @@ if __name__ == '__main__':
     # The main, opens file and tails it every time a keyword is found
     # adds/removes an user
 
+    # Parse arguments
+    parser = argparse.ArgumentParser(description='Description of your program')
+    parser.add_argument('-f', '--file', help='Log File path', required=True)
+    args = vars(parser.parse_args())
+
+    # set arguments to variables
+    if args['file']:
+        my_log_file_path = args['file']
+
     # Init variables
     time_now = datetime.now()
     START_UP_TIME = time_now.strftime("%m/%d/%Y, %H:%M:%S")
 
-    logfile = open("x.log", "r")
+    # Open the log file
+    try:
+        logfile = open(my_log_file_path)
+    except IOError:
+        print(IOError)
+        sys.exit()
+
     loglines = follow(logfile)
 
     # clean up html on start
